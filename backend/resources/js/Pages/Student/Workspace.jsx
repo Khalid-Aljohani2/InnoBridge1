97ae53c467da059ceae44831e4b8920b0474bede
Khaled Fahad <Kh.almass3dii@gmail.com>
2026-05-18 15:25:51 +0300

    fix: resolve esbuild compile error in Workspace.jsx due to mismatched JSX closing tag

--- backend/resources/js/Pages/Student/Workspace.jsx
+++ backend/resources/js/Pages/Student/Workspace.jsx
@@ -342,7 +342,7 @@ export default function Workspace({ team = null, milestones = [], isPendingRevie
                                 </div>
                             </div>
                         </div>
-                    </div>
+                    )}
                 </div>
             </div>
         </AuthenticatedLayout>
